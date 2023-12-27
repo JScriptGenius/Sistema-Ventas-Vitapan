be2a4091835e6c244e86b32d71afff725179e00d
Jhosep <joseph@scriptgenius.com>
2023-12-27 01:08:21 -0500

    Primer punto de partida y configuración del archivo application properties

---
+++ src/main/java/com/sistema/ventas/vitapan/SistemaVentasVitapanApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.ventas.vitapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentasVitapanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentasVitapanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/ventas/vitapan/SistemaVentasVitapanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.ventas.vitapan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVentasVitapanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
