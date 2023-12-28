776bf26b0bec808b66d86ad4db54fc565be72260
Jhosep <joseph@scriptgenius.com>
2023-12-28 12:47:14 -0500

    Codificación de endpoints para realizar tareas CRUD en el controlador Cliente y funcionalidad en las vistas de cliente

--- src/main/java/com/sistema/ventas/vitapan/controller/ClienteController.java
+++ src/main/java/com/sistema/ventas/vitapan/controller/ClienteController.java
@@ -1,11 +1,12 @@
 package com.sistema.ventas.vitapan.controller;
 
+import com.sistema.ventas.vitapan.entities.Cliente;
 import com.sistema.ventas.vitapan.repository.ClienteRepository;
 import com.sistema.ventas.vitapan.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cliente")
@@ -17,13 +18,48 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @GetMapping("/lista-clientes")
-    public String listaClientes() {
+    @GetMapping("/listaClientes")
+    public String listaClientes(Model model) {
+        model.addAttribute("clientes",clienteRepository.findAllClientes());
         return "/cliente/listaClientes";
     }
 
-    @GetMapping("/registrar-cliente")
+    @GetMapping("/nuevoCliente")
     public String registrarCliente() {
         return "/cliente/registrarCliente";
     }
+
+    @PostMapping("/registrarCliente")
+    public String registrarCliente(Cliente cliente) {
+        cliente.setEstado(1);
+        clienteService.saveCliente(cliente);
+        return "redirect:/cliente/listaClientes";
+    }
+
+    @GetMapping("/editarCliente/{idCliente}")
+    public String editarCliente(@PathVariable Long idCliente, Model model) {
+        Cliente cliente = clienteService.findClienteById(idCliente).get();
+        model.addAttribute("cliente",cliente);
+        return "/cliente/actualizarCliente";
+    }
+
+    @PostMapping("/actualizarCliente")
+    public String actualizarCliente(@RequestParam("idCliente") Long id, Cliente cliente) {
+        Cliente clienteBD = clienteService.findClienteById(id).get();
+        clienteBD.setDni(cliente.getDni());
+        clienteBD.setNombre(cliente.getNombre());
+        clienteBD.setApellido(cliente.getApellido());
+        clienteBD.setDireccion(cliente.getDireccion());
+        clienteBD.setTelefono(cliente.getTelefono());
+        cliente.setEstado(clienteBD.getEstado());
+        clienteService.updateCliente(cliente);
+        return "redirect:/cliente/listaClientes";
+    }
+
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(@PathVariable Long idCliente) {
+        clienteRepository.disableCliente(idCliente);
+        return "redirect:/cliente/listaClientes";
+    }
+
 }
